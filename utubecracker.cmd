0e4c9660a389aba1cf8831167574b63c62ee28f4
apiphon <74528989+apiphon@users.noreply.github.com>
2021-01-17 22:31:45 +0700

    Create utubecracker.cmd

---
+++ utubecracker.cmd
@@ -0,0 +1,17 @@
+@echo off
+title youtube cracker
+cls
+color 0A
+echo youtube cracker v2.01
+echo Create by Apiphon Suwanchaisakul
+echo:
+SET/P "namevid=Enter VIDEO URL : "
+set/p "loopvid=Enter Loop VIDEO : "
+set loopnow=0
+:punvilled
+start chrome.exe "%namevid%"
+set /a loopnow=%loopnow%+1
+if %loopnow%==%loopvid% goto loopfinishvid
+goto punvilled
+:loopfinishvid
+exit
